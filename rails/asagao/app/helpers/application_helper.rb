f15d61a993fe69e8c3db569e5c0d078e287e96eb
NotReady <loreley.lala@gmail.com>
2018-01-02 02:19:33 +0900

    レイアウトの実装

--- rails/asagao/app/helpers/application_helper.rb
+++ rails/asagao/app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 module ApplicationHelper
+  
+  # ページタイトルを取得します
   def page_title
     title = "Morning Glory"
     title = @page_title + " - " + title if @page_title
     title
+  end
+  
+  # メニューリンクを作成します
+  def menu_link_to(text, path)
+    link_to_unless_current(text, path) do
+      content_tag(:span, text)
+    end
   end
 end
