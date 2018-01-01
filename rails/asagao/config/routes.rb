baf2c38d95a76372d16bb2ffba373a1735708496
NotReady <loreley.lala@gmail.com>
2018-01-01 17:42:44 +0900

    ルーティングの基礎

--- rails/asagao/config/routes.rb
+++ rails/asagao/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  root "top#index"
+
+  # ルートのルーティング
+  # root('Controller#Action')
+  root "top#index"  
+
+  # GETメソッドによる/aboutのルーティング
+  # get(パス => "Controller#Action", as: "ルーティング名")
+  # /aboutのアクセス⇒ TopController.aboutの関連と、abount_pathメソッド⇒/aboutパスの取得
+  get "about" => "top#about", as: "about" 
+
 end
