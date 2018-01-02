1adf800511e2dd1423e24b270a183d6f3f45fa1c
NotReady <loreley.lala@gmail.com>
2018-01-02 11:09:36 +0900

    シードデータの投入

--- rails/asagao/db/seeds.rb
+++ rails/asagao/db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# db/seeds/development　にテーブル名.rbがあれば実行する
+table_names = %w(members)
+table_names.each do |table_name|
+	path = Rails.root.join("db/seeds", Rails.env, table_name + ".rb")
+	if File.exist?(path)
+		puts "Creating #{table_name}..."
+		require path
+	end
+end
