aeb5a35ea8bc44d069f239692276b86f3abea2c2
NotReady <loreley.lala@gmail.com>
2018-01-04 17:05:51 +0900

    FactoryGirlの利用

--- rails/asagao/test/models/member_test.rb
+++ rails/asagao/test/models/member_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
+require 'factory_girl_rails'
 
 class MemberTest < ActiveSupport::TestCase
-  test 'the truth' do
-    assert false
+  test 'factory girl' do
+    member = FactoryGirl.create(:member)
+    assert_equal 'Yamada Taro', member.full_name
   end
 end
