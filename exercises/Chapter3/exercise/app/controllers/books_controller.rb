1a8ddd98e3f7dc7eea630c611c740690e05ed5cb
NotReady <loreley.lala@gmail.com>
2018-01-20 19:44:10 +0900

    Chapter4 練習問題

---
+++ exercises/Chapter3/exercise/app/controllers/books_controller.rb
@@ -0,0 +1,5 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.find(1)
+  end
+end
--- exercises/Chapter3/exercise/config/routes.rb
+++ exercises/Chapter3/exercise/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'books/index'
+
   get "Chapter3" => "chapter3controller#index" 
   get "top" => "chapter3controller#top", as: "top"
 end
